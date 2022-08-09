b7cb52dee68bf94919f186c23ee0e570cd70d5d2
mohan <sahumohan338@gmail.com>
2022-08-09 00:48:15 -0700

    [mohan]add: Max number in integer

---
+++ GenericsProblem/GenericsProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericsProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MaxNumber problem = new MaxNumber();
+            //First case 
+            problem.FindMax(22,15,8);
+            //Second case
+            problem.FindMax(4,28,9);
+            //Third case
+            problem.FindMax(5,10,17);
+          
+        }
+    }
+}
